3e3c412dad73d2b32f9834af757b744ecff6aa0b
Titan1285 <austingarrick752@gmail.com>
2026-08-12 20:35:32 -0700

    Revert EHCI patch, and switch to XHCI for mixed speed support

--- hw/usb/dev-serial.c
+++ hw/usb/dev-serial.c
@@ -135,18 +135,18 @@ static const USBDescIface desc_iface0 = {
         {
             .bEndpointAddress      = USB_DIR_IN | 0x01,
             .bmAttributes          = USB_ENDPOINT_XFER_BULK,
-            .wMaxPacketSize        = 512,
+            .wMaxPacketSize        = 64,
         },{
             .bEndpointAddress      = USB_DIR_OUT | 0x02,
             .bmAttributes          = USB_ENDPOINT_XFER_BULK,
-            .wMaxPacketSize        = 512,
+            .wMaxPacketSize        = 64,
         },
     }
 };
 
 static const USBDescDevice desc_device = {
     .bcdUSB                        = 0x0200,
-    .bMaxPacketSize0               = 64,
+    .bMaxPacketSize0               = 8,
     .bNumConfigurations            = 1,
     .confs = (USBDescConfig[]) {
         {
@@ -169,7 +169,7 @@ static const USBDesc desc_serial = {
         .iProduct          = STR_PRODUCT_SERIAL,
         .iSerialNumber     = STR_SERIALNUMBER,
     },
-    .high = &desc_device,
+    .full = &desc_device,
     .str  = desc_strings,
 };
 
